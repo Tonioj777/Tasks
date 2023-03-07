7cbe0152b8ac0436c219d01bef1da6ae95ff2bd8
Tonioj777 <125373133+Tonioj777@users.noreply.github.com>
2023-03-07 20:30:19 +0300

    Add files via upload

---
+++ Lesson 35 Вертска Header/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    $(".rateYo").rateYo({
+        starWidth: "35px"
+        
+    });
+
+    $(".rateYo").rateYo({
+        normalFill: "#D7D1C7"
+    });
+
+    $(".rateYo").rateYo({
+        ratedFill: "#E74C3C"
+      });
+
+
+
+
+
+
+
+
+});
+
+
+
